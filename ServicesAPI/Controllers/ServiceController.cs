c68bb768833752fd788cd2ec0c556a29c2ad136c
apk59 <apk59@SMART>
2022-10-31 15:32:22 +0300

    Added service controller

---
+++ ServicesAPI/Controllers/ServiceController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicesAPI.Core.Entities.DataTransferObject;
+using ServicesAPI.Core.Services.Abstractions;
+
+namespace ServicesAPI.Controllers
+{
+    [Route("api/service")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public ServiceController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetServices()
+        {
+            var servicesDto = await _serviceManager.Service.GetAllServicesAsync();
+
+            return Ok(servicesDto);
+        }
+
+        [HttpGet("{serviceId:guid}")]
+        public async Task<IActionResult> GetServiceById(Guid serviceId)
+        {
+            var serviceDto = await _serviceManager.Service.GetServiceAsync(serviceId);
+
+            return Ok(serviceDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateService([FromBody] ServiceForCreationDto serviceForCreationDto)
+        {
+            var serviceDto = await _serviceManager.Service.CreateService(serviceForCreationDto);
+
+            return CreatedAtAction(nameof(GetServiceById), new {serviceId = serviceDto.Id}, serviceDto);
+        }
+
+        [HttpPut("{serviceId:guid}")]
+        public async Task<IActionResult> UpdateService(Guid serviceId, [FromBody] ServiceForUpdateDto serviceForUpdateDto)
+        {
+            await _serviceManager.Service.UpdateService(serviceId, serviceForUpdateDto);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{serviceId:guid}")]
+        public async Task<IActionResult> DeleteService(Guid serviceId)
+        {
+            await _serviceManager.Service.DeleteService(serviceId);
+
+            return NoContent();
+        }
+    }
+}
