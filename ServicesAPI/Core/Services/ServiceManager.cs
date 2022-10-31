3fed194e537fc325830a0a924259ec255a97687c
apk59 <apk59@SMART>
2022-10-31 17:48:39 +0300

    Update service manager

--- ServicesAPI/Core/Services/ServiceManager.cs
+++ ServicesAPI/Core/Services/ServiceManager.cs
@@ -9,13 +9,15 @@ namespace ServicesAPI.Core.Services
     public sealed class ServiceManager : IServiceManager
     {
         private readonly Lazy<IServiceService> _lazyServiceService;
+        private readonly Lazy<IServiceCategoryService> _lazyServiceCategoryService;
 
         public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper)
         {
-            _lazyServiceService = new Lazy<IServiceService>(() => new ServiceService(repositoryManager, mapper));   
+            _lazyServiceService = new Lazy<IServiceService>(() => new ServiceService(repositoryManager, mapper));
+            _lazyServiceCategoryService = new Lazy<IServiceCategoryService>(() => new ServiceCategoryService(repositoryManager, mapper));
         }
 
-        public IServiceCategoryService CategoryService => throw new NotImplementedException();
+        public IServiceCategoryService CategoryService => _lazyServiceCategoryService.Value;
 
         public IServiceService Service => _lazyServiceService.Value;
     }
