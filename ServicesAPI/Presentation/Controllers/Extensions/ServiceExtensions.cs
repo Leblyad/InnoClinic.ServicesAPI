9cd1c3c5e27f08ea57979cacba9f8abde7e15b07
S9392929 <80668687+Leblyad@users.noreply.github.com>
2022-11-08 12:28:47 +0300

    Fix glogal exception handler

--- ServicesAPI/Presentation/Controllers/Extensions/ServiceExtensions.cs
+++ ServicesAPI/Presentation/Controllers/Extensions/ServiceExtensions.cs
@@ -38,5 +38,26 @@ namespace ServicesAPI.Extensions
         {
             services.AddTransient<ExceptionHandlingMiddleware>();
         }
+
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, )
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
     }
 }
