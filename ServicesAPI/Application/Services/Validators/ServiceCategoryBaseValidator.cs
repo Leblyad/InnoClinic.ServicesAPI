10510c822fad57ced233dc3d00df645e06cbe730
S9392929 <80668687+Leblyad@users.noreply.github.com>
2022-11-10 10:18:43 +0300

    Add fluent validation and fix connection string

---
+++ ServicesAPI/Application/Services/Validators/ServiceCategoryBaseValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ServicesAPI.Core.Entities.DataTransferObject;
+
+namespace ServicesAPI.Application.Services.Validation
+{
+    public class ServiceCategoryBaseValidator<T> : AbstractValidator<T> where T : ServiceCategoryForManipulationDto
+    {
+        public ServiceCategoryBaseValidator()
+        {
+            RuleFor(x => x.CategoryName).Length(2, 30);
+        }
+    }
+}
