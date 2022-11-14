2142dd3dfe6290600f2cfac8a35d5d45024684ee
S9392929 <80668687+Leblyad@users.noreply.github.com>
2022-11-14 13:02:04 +0300

    Align some lines and change names of entities

--- ServicesAPI/Infrastructure/Repository/UserClasses/ServiceCategoryRepository.cs
+++ ServicesAPI/Infrastructure/Repository/UserClasses/ServiceCategoryRepository.cs
@@ -25,21 +25,21 @@ namespace ServicesAPI.Core.Repository.UserClasses
             await RepositoryContext.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<ServiceCategory>> GetAllServiceCategoriesAsync(ServiceCategoryParameters serviceCategoryParameters, bool trackChanges) =>
+        public async Task<IEnumerable<ServiceCategory>> GetAllServiceCategoriesAsync(ServiceCategoryParameters serviceCategoryParameters, bool trackChanges = false) =>
             serviceCategoryParameters.PageNumber > 0
             && serviceCategoryParameters.PageSize > 0 ?
             await FindAll(trackChanges)
-            .OrderBy(category => category.CategoryName)
-            .Skip((serviceCategoryParameters.PageNumber - 1) * serviceCategoryParameters.PageSize)
-            .Take(serviceCategoryParameters.PageSize)
-            .ToListAsync() :
+                .OrderBy(category => category.CategoryName)
+                .Skip((serviceCategoryParameters.PageNumber - 1) * serviceCategoryParameters.PageSize)
+                .Take(serviceCategoryParameters.PageSize)
+                .ToListAsync() :
              await FindAll(trackChanges)
-            .OrderBy(category => category.CategoryName)
-            .ToListAsync();
+                .OrderBy(category => category.CategoryName)
+                .ToListAsync();
 
 
-        public async Task<ServiceCategory> GetServiceCategoryAsync(Guid serviceCategoryId, bool trackChanges) =>
+        public async Task<ServiceCategory> GetServiceCategoryAsync(Guid serviceCategoryId, bool trackChanges = false) =>
             await FindByCondition(serviceCategory => serviceCategory.Id.Equals(serviceCategoryId), trackChanges)
-            .SingleOrDefaultAsync();
+                .SingleOrDefaultAsync();
     }
 }
